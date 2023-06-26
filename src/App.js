ddd70d826146c57cf719fc428463c811af3ff1dd
Prayush <dpratyush412@gmail.com>
2023-06-26 20:42:46 +0530

    Initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LandingPage from './Components/LandingPage';
+function App() {
+  return (
+    <div className="App">
+  <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='container'>
+      <div className="header"><p>Drive</p> <p>Gaurd</p></div>
+      <div className="main-content">
+      <div className="text-part">
+        <p>Welcome</p>
+        <p>to</p>
+        <p>DriveGuard</p>
+        <button className='btn' >Get started</button>
+        </div>
+        <div className="image-part"></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default LandingPage
