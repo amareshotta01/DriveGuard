32a737482147c0d596fb17576d3ffa812a46f76e
Prayush <dpratyush412@gmail.com>
2023-06-27 20:42:52 +0530

    Landing Page commit

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import './LandingPage.css'
+import driveGuardImage from '../Images/DriveGaurd1.jpg';
+import D from '../Images/5a01ba7a7ca233f48ba627a8.png';
 
 const LandingPage = () => {
   return (
-    <div className='container'>
-      <div className="header"><p>Drive</p> <p>Gaurd</p></div>
+    <div className='container'> 
       <div className="main-content">
       <div className="text-part">
-        <p>Welcome</p>
-        <p>to</p>
-        <p>DriveGuard</p>
+      <div className="header"><img src={D} alt="" className='logo'/><p>rive</p> <p>Gaurd</p></div>
+        <p className='text'>Welcome</p>
+        <p className='text'>to</p>
+        <p className='text'>DriveGuard</p>
         <button className='btn' >Get started</button>
         </div>
-        <div className="image-part"></div>
+        <div className="image-part">
+            <img src={driveGuardImage} alt="" />
+        </div>
         </div>
 
     </div>
