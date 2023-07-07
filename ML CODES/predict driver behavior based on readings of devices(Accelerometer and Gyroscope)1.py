91f3319d69f3284b6657e5b5f86dc3083aa14143
Prayush <dpratyush412@gmail.com>
2023-07-07 11:02:10 +0530

    mlcode

--- ML CODES/predict driver behavior based on readings of devices(Accelerometer and Gyroscope)1.py
+++ ML CODES/predict driver behavior based on readings of devices(Accelerometer and Gyroscope)1.py
@@ -1,72 +1,65 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
+app = Flask(__name__)
 
-# In[4]:
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Read accelerometer and gyroscope data
+    accelerometer_data = pd.read_csv('Accelerometer.csv', delimiter=',')
+    gyroscope_data = pd.read_csv('Gyroscope.csv', delimiter=',')
 
+    # Merge based on timestamp and milliseconds
+    merged_data = pd.merge(accelerometer_data, gyroscope_data, on=['Timestamp', 'Milliseconds'])
 
+    # Calculate the magnitude of the acceleration vector
+    merged_data['accel_magnitude'] = (merged_data['X_accel'] ** 2 + merged_data['Y_accel'] ** 2 + merged_data['Z_accel'] ** 2) ** 0.5
 
-# accelerometer and gyroscope data 
-accelerometer_data = pd.read_csv('Accelerometer.csv', delimiter=',')
-gyroscope_data = pd.read_csv('Gyroscope.csv', delimiter=',')
+    # Threshold values for each behavior
+    aggressive_threshold = 10.0
+    risky_threshold = 15.0
 
-# Merge based on timestamp and milliseconds
-merged_data = pd.merge(accelerometer_data, gyroscope_data, on=['Timestamp', 'Milliseconds'])
+    # Assign labels based on the magnitude
+    merged_data.loc[merged_data['accel_magnitude'] < aggressive_threshold, 'label'] = 'Normal'
+    merged_data.loc[(merged_data['accel_magnitude'] >= aggressive_threshold) & (merged_data['accel_magnitude'] < risky_threshold), 'label'] = 'Aggressive'
+    merged_data.loc[merged_data['accel_magnitude'] >= risky_threshold, 'label'] = 'Risky'
 
-# magnitude of acceleration vector
-merged_data['accel_magnitude'] = (merged_data['X_accel']**2 + merged_data['Y_accel']**2 + merged_data['Z_accel']**2)**0.5
+    # Split into training and testing sets
+    X = merged_data[['X_accel', 'Y_accel', 'Z_accel', 'X_gyro', 'Y_gyro', 'Z_gyro']]
+    y = merged_data['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# threshold values for each behavior (u take on ur own acoording to u)
-aggressive_threshold = 10.0
-risky_threshold = 15.0
+    # Random Forest Classifier
+    rf_classifier = RandomForestClassifier()
 
-# labels based on the magnitude 
-merged_data.loc[merged_data['accel_magnitude'] < aggressive_threshold, 'label'] = 'Normal'
-merged_data.loc[(merged_data['accel_magnitude'] >= aggressive_threshold) & (merged_data['accel_magnitude'] < risky_threshold), 'label'] = 'Aggressive'
-merged_data.loc[merged_data['accel_magnitude'] >= risky_threshold, 'label'] = 'Risky'
+    # Train the model
+    rf_classifier.fit(X_train, y_train)
 
-# Split  into training and testing 
-X = merged_data[['X_accel', 'Y_accel', 'Z_accel', 'X_gyro', 'Y_gyro', 'Z_gyro']]
-y = merged_data['label']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Get the input values from the form
+    x_accel = float(request.form['x_accel'])
+    y_accel = float(request.form['y_accel'])
+    z_accel = float(request.form['z_accel'])
+    x_gyro = float(request.form['x_gyro'])
+    y_gyro = float(request.form['y_gyro'])
+    z_gyro = float(request.form['z_gyro'])
 
-# Random Forest Classifier
-rf_classifier = RandomForestClassifier()
+    # Create a new data point for prediction
+    new_data = pd.DataFrame({'X_accel': [x_accel], 'Y_accel': [y_accel], 'Z_accel': [z_accel], 'X_gyro': [x_gyro], 'Y_gyro': [y_gyro], 'Z_gyro': [z_gyro]})
 
-# Train 
-rf_classifier.fit(X_train, y_train)
+    # Use the trained model to predict the category of the new data point
+    predicted_category = rf_classifier.predict(new_data)
 
-# Predict 
-y_pred = rf_classifier.predict(X_test)
+    return render_template('index.html', prediction=predicted_category[0])
 
-#  performance
-print(classification_report(y_test, y_pred))
-
-
-# In[5]:
-
-
-import pandas as pd
-
-# ur new  data point
-new_data = pd.DataFrame({'X_accel': [0.5], 'Y_accel': [-0.3], 'Z_accel': [0.2], 'X_gyro': [0.1], 'Y_gyro': [-0.1], 'Z_gyro': [0.3]})
-
-#the trained model now  predict the category
-predicted_category = rf_classifier.predict(new_data)
-
-print(predicted_category)
-
-
-# In[ ]:
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
 
 
 
