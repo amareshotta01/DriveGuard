dd57c90ef708717cc8afd368f44b4fe324777f7f
Priyadarshini Bal <pbal2003@gmail.com>
2023-07-08 23:16:03 +0530

    Merge remote changes into my local branch

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -3,6 +3,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import './HomePage.css';
 import { account } from '../../Appwrite/appwrite.config';
 import { Link } from 'react-router-dom';
+
+// import { library } from '@fortawesome/fontawesome-free';
+// import { faFacebookF, faInstagram, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+// library.add(faFacebookF, faInstagram, faTwitter, faLinkedinIn);
+
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -68,6 +73,94 @@ function HomePage() {
 
       <div className='services-container' id="services">
         <div className="services-heading">Services</div>
+        <div className='container'>
+          {/* service item box */}
+          <div className='seivice-items'>
+            <h3>Voice Recognition</h3>
+            <p>Monitors and analyze voice interactions between passengers and drivers within the ride-hailing platform.</p>
+            <button>Click here</button>
+          </div>
+
+          <div className='seivice-items'>
+            <h3>Intelligent Trip Monitoring</h3>
+            <p>Utilizes real-time data from sensor and monitors driving behavior or potentially unsafe actions.</p>
+            <button>Click here</button>
+          </div>
+
+          <div className='seivice-items'>
+            <h3>Safety Analytics Dashboard</h3>
+            <p>This dashboard enables ride-hailing companies to identify patterns, uncover areas for improvement, and implement proactive measures to enhance ride safety.</p>
+            <button>Click here</button>
+          </div>
+
+          <div className='seivice-items'>
+            <h3>Gamification System</h3>
+            <p>This system rewards drivers for consistently adhering to safety protocols and receiving positive feedback from passengers</p>
+            <button>Click here</button> 
+          </div>
+
+        </div>
+      </div>
+
+      <div>
+        <footer>
+          <div className='footer-content'>
+            
+            <div className='left box'>
+              <h2>Connect us</h2>
+              <div className='content'>
+                <p>Also connect us on differnt social media platforms</p>
+                <div className='social'>
+                  <a herf="#"><span class="fab fa-facebook-f"></span></a>
+                  <a herf="#"><span class="fab fa-instagram"></span></a>
+                  <a herf="#"><span class="fab fa-twitter"></span></a>
+                  <a herf="#"><span class="fab fa-linkedin-in"></span></a>
+                </div>
+              </div>
+            </div>
+
+            <div className='center box'>
+              <h2>Address</h2>
+              <div className='content'>
+
+                <div className='place'>
+                  <span className='fas fa-map-marker-alt'></span>
+                  <span className='text'>Sambalpur,Odisha</span>
+                </div>
+
+                <div className='phone'>
+                  <span className='fas fa-phone-alt'></span>
+                  <span className='text'>+91 7890654213</span>
+                </div>
+
+                <div className='email'>
+                  <span className='fas fa-envelope'></span>
+                  <span className='text'>driveguard@gmail.com</span>
+                </div>
+
+              </div>
+            </div>
+
+            <div className='right box'>
+              <h2>Contact us</h2>
+              <div className='content'>
+                <form action="#">
+                  <div className='email'>
+                    <div className='text'>Email</div>
+                    <input type="email" id="email" required aria-required="true" />
+                  </div>
+                  <div className='msg'>
+                    <div className='text'>Message</div>
+                    <textarea rows={2} cols={25} required aria-required="true" />
+                  </div>
+                  <div className='btn'>
+                    <button type="submit" className='btn'>Send</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
       </div>
       
